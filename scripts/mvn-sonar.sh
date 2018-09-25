11cd5486e61cc46cdbe6b5bbe992501d47d353da
Yi Yang <Yi_Yang@StateStreet.com>
2018-09-25 16:45:32 -0400

    Updated

---
+++ scripts/mvn-sonar.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e -x 
+
+cd /updated-sample-app
+./mvnw "sonar:sonar -Dsonar.host.url=http://host.docker.internal:9000" 
--- scripts/mvn-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e 
-set -x 
-
-cd /updated-sample-app
-./mvnw "surefire:test"
