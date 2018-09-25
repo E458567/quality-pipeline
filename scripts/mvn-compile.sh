c043fa8af3101990e4d375e35e1c4aa03285b13d
Yi Yang <Yi_Yang@StateStreet.com>
2018-09-25 15:07:30 -0400

    Updated again

--- scripts/mvn-compile.sh
+++ scripts/mvn-compile.sh
@@ -6,4 +6,6 @@ set -x
 ls -al git-sample-app
 
 git clone git-sample-app updated-sample-app 
-./updated-sample-app/mvnw compile test-compile
+cd updated-sample-app
+./mvnw compile test-compile
+cd ..
