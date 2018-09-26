fd5a59e770c244d9855974875de36d9f30a66153
Yi Yang <Yi_Yang@StateStreet.com>
2018-09-25 20:18:26 -0400

    Updated to use sonar cloud

--- scripts/mvn-sonar.sh
+++ scripts/mvn-sonar.sh
@@ -3,4 +3,8 @@
 set -e -x 
 
 cd updated-sample-app
-./mvnw "sonar:sonar -Dsonar.host.url=http://host.docker.internal:9000" 
+./mvnw sonar:sonar \
+  -Dsonar.projectKey=sample-application \
+  -Dsonar.organization=e458567-github \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=dc0441ab630b639fd650949bf4e9750295a93faa
