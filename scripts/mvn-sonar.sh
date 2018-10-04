e7d5fae790bcaf64d542dbeaaae0330d8dee8bb3
Yi Yang <Yi_Yang@StateStreet.com>
2018-10-04 11:40:19 -0400

    Updated sonar URL

--- scripts/mvn-sonar.sh
+++ scripts/mvn-sonar.sh
@@ -4,7 +4,8 @@ set -e -x
 
 cd updated-sample-app
 ./mvnw sonar:sonar \
-  -Dsonar.projectKey=sample-application \
-  -Dsonar.organization=e458567-github \
-  -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=dc0441ab630b639fd650949bf4e9750295a93faa
+  -Dsonar.host.url=http://192.168.64.3:32224
+#  -Dsonar.projectKey=sample-application \
+#  -Dsonar.organization=e458567-github \
+#  -Dsonar.host.url=https://sonarcloud.io \
+#  -Dsonar.login=dc0441ab630b639fd650949bf4e9750295a93faa
